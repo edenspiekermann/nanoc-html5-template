90184f54f6d79bd3eff6aacd6a3d5f27c81fea7a
Dirk Schmid <d.schmid@de.edenspiekermann.com>
2010-09-10 12:33:37 +0200

    adding menue

--- lib/default.rb
+++ lib/default.rb
@@ -1,6 +1,7 @@
 require 'active_support/core_ext'
 require 'app/helpers/html5_boilerplate_helper'
 
+include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Html5BoilerplateHelper
 
